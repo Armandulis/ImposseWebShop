5b62d956d9cb484253d4bdf0f9e5fc7ea033408c
Armandulis <Armandulis@gmail.com>
2018-11-27 11:33:39 +0100

    OnionBluePrint
    Architecture

---
+++ WebShop.Infrastructure.Data/DBSeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Infrastructure.Data
+{
+    public class DBSeed
+    {
+        public static void SeedDB(WebShopContext ctx)
+        {   
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+
+
+        }
+
+    }
+}
---
+++ WebShop.Infrastructure.Data/WebShopContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Infrastructure.Data
+{
+   public class WebShopContext : DbContext
+    {
+        public WebShopContext(DbContextOptions<WebShopContext> option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+
+    }
+}
